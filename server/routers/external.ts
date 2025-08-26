f7017115357fc6f8fa4fc9085e5dfa5c9d46988d
Pallavi <pallavilpmt1995@gmail.com>
2025-08-26 22:35:57 +0530

    transferResource error fix

--- server/routers/external.ts
+++ server/routers/external.ts
@@ -499,13 +499,6 @@ authenticated.get(
 );
 
 authenticated.post(
-    `/resource/:resourceId/transfer`,
-    verifyResourceAccess,
-    verifyUserHasAction(ActionsEnum.updateResource),
-    resource.transferResource
-);
-
-authenticated.post(
   `/resource/:resourceId/move-org`,
   verifyResourceAccess,
   verifyUserHasAction(ActionsEnum.updateResource),
