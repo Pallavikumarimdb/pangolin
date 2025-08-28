9fa73cb4d6ae72c43b52d6048f422edfb909aba3
Pallavi <pallavilpmt1995@gmail.com>
2025-08-28 20:59:48 +0530

    remove auto date update

--- server/routers/resource/updateResource.ts
+++ server/routers/resource/updateResource.ts
@@ -450,7 +450,6 @@ async function updateResourceHostnames(
                     fullDomain: processedHostname.data!.fullDomain,
                     baseDomain: processedHostname.data!.baseDomain,
                     primary: processedHostname.data!.primary,
-                    createdAt: new Date().toISOString()
                 });
             }
         });
