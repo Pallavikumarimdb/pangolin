32726a3115a66437a36dfb4658a34e80129a6d99
Pallavi <pallavilpmt1995@gmail.com>
2025-09-01 04:16:54 +0530

    update schema to make siteId nullable

--- server/db/sqlite/schema.ts
+++ server/db/sqlite/schema.ts
@@ -119,8 +119,7 @@ export const targets = sqliteTable("targets", {
     siteId: integer("siteId")
         .references(() => sites.siteId, {
             onDelete: "cascade"
-        })
-        .notNull(),
+        }),
     ip: text("ip").notNull(),
     method: text("method"),
     port: integer("port").notNull(),
