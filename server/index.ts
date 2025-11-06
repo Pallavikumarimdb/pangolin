df1a00d449c4e251eb9857cd787417cf254e68e2
Pallavi Kumari <pallavilpmt1995@gmail.com>
2025-11-06 20:27:24 +0530

    point the resource to the nextjs server for maintenance screen

--- server/index.ts
+++ server/index.ts
@@ -22,7 +22,6 @@ import { initCleanup } from "#dynamic/cleanup";
 import license from "#dynamic/license/license";
 import { initLogCleanupInterval } from "@server/lib/cleanupLogs";
 import { fetchServerIp } from "@server/lib/serverIpService";
-import { startMaintenanceServer } from "./maintenance/maintenance-server.js";
 
 async function startServers() {
     await setHostMeta();
@@ -54,8 +53,6 @@ async function startServers() {
     if (config.getRawConfig().flags?.enable_integration_api) {
         integrationServer = createIntegrationApiServer();
     }
-
-     startMaintenanceServer();
 
     await initCleanup();
 
